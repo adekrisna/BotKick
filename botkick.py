5594d8715f265b6ec3ddff63d857a3572aef7bba
Syams225 <ahmadsyams225@gmail.com>
2017-10-02 17:58:09 +0700

    Update botkick.py

--- botkick.py
+++ botkick.py
@@ -126,11 +126,11 @@ def SEND_MESSAGE(op):
             if msg.contentType == 0:
                 #if "gname:" in msg.text:
 #--------------------------------------------------------------
-                if msg.text == "Mulai":
+                if msg.text == "Kick":
                     print "ok"
-                    _name = msg.text.replace("Mulai","")
+                    _name = msg.text.replace("Kick","")
                     gs = client.getGroup(msg.to)
-                    sendMessage(msg.to,"Halooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
+                    sendMessage(msg.to,"Waktunya Untuk Meng Kick Orang Di Group Ini Anjay Wkwk")
                     targets = []
                     for g in gs.members:
                         if _name in g.displayName:
